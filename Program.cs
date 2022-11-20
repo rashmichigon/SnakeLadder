85f65f3fa8fa948e6df02e1ce811f782df1d2d74
Rashmi <rashmichigon65@gmail.com>
2022-11-20 09:21:24 +0530

    SnakeLadderUC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Simulation");
+            int position = 0;
+            Console.WriteLine("Initial (start) position is:" + position);
+            Console.ReadLine();
         }
     }
 }
