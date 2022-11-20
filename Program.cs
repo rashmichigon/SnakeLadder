ad58a98693713cbbb77688b1ff87499d1c6968ed
Rashmi <rashmichigon65@gmail.com>
2022-11-20 12:22:00 +0530

    SnakeLadderUC7

--- Program.cs
+++ Program.cs
@@ -1,46 +1,56 @@
-﻿namespace SnakeLadder
+﻿using System.Numerics;
+
+namespace SnakeLadder
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             const int FirstPosition = 0;
-            int position = 0;
-            int DiceRoll;
-            Console.WriteLine("First player is starting with position " + FirstPosition);
-            while (position < 100)
+            const int SecondPosition = 0;
+            int position1 = 0;
+            int position2 = 0;
+            int DiceRoll1, DiceRoll2;
+            int count = 0;
+            Console.WriteLine("Player 1 is starting with position" + FirstPosition);
+            Console.WriteLine("Player 2 is starting with position" + SecondPosition);
+            while (position1 < 100 || position2 < 100)
             {
-                Random random = new Random();
-                DiceRoll = random.Next(1, 7);
-                Console.WriteLine("The number player got through dice is" + DiceRoll);
+                count++;
+                Random random1 = new Random();
+                DiceRoll1 = random1.Next(1, 7);
                 Random random2 = new Random();
-                int play = random2.Next(1, 4);
-                if (play == 1)
+                DiceRoll2 = random2.Next(1, 7);
+                Console.WriteLine("The number player 1 got through dice is" + DiceRoll1);
+                Console.WriteLine("The number player 2 got through dice is" + DiceRoll2);
+                Random random3 = new Random();
+                int play1 = random3.Next(1, 4);
+                Random random4 = new Random();
+                int play2 = random4.Next(1, 4);
+                if (play1 == 1)
                 {
-                    Console.WriteLine("Player got no play");
-                    DiceRoll = 0;
-                    position = position + DiceRoll;
+                    Console.WriteLine("Player 1 got no play");
+                    DiceRoll1 = 0;
+                    position1 = position1 + DiceRoll1;
                 }
-                else if (play == 2)
+                else if (play1 == 2)
                 {
-                    Console.WriteLine("Player got ladder");
-                    position = (position + DiceRoll);
+                    Console.WriteLine("Player1 got ladder");
+                    position1 = (position1 - DiceRoll1);
                 }
                 else
                 {
-                    Console.WriteLine("Player got snake");
-                    position = (position - DiceRoll);
-                    if (position < 0)
+                    Console.WriteLine("Player1 got snake");
+                    position1 = (position1 - DiceRoll1);
+                    if (position1 < 0)
                     {
-                        position = FirstPosition;
+                        position1 = FirstPosition;
                     }
-                    if (position > 100)
-                    {
-                        position = position - DiceRoll;
-                    }
-                    Console.WriteLine("The Player position is: " + position);
                 }
-                Console.ReadLine();
+                if (position1 > 100)
+                {
+                    position1 = position1 - DiceRoll1;
+                }
             }
 
         }
