6423b765a1b52c7df75bd93db5d9ead3e91f9165
Rashmi <rashmichigon65@gmail.com>
2022-11-20 09:15:26 +0530

    SnakeLadder

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Simulation");
+        }
+    }
+}
