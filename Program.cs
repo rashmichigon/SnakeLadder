e8cf1fb923e087a7c56370387808242cf9fd1ca4
Rashmi <rashmichigon65@gmail.com>
2022-11-20 11:34:01 +0530

    SnakeLadderUC3

--- Program.cs
+++ Program.cs
@@ -16,6 +16,32 @@
             Random random = new Random();
             int diceOutput = random.Next(1, 7);
             Console.WriteLine("Dice Output is :" + diceOutput);
+
+            //UC3
+
+            position = diceOutput;
+            Console.WriteLine("Position :", position);
+            Random random2 = new Random();
+            Console.WriteLine("Checking for Options");
+            int option = random2.Next(1, 4);
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("No Play");
+                    position = position;
+                    Console.WriteLine("Position :" + position);
+                    break;
+                case 2:
+                    Console.WriteLine("Ladder");
+                    position = diceOutput;
+                    Console.WriteLine("Position :" + position);
+                    break;
+                case 3:
+                    Console.WriteLine("Snake");
+                    position = diceOutput;
+                    Console.WriteLine("Position :" + position);
+                    break;
+            }
             Console.ReadLine();
         }
     }
