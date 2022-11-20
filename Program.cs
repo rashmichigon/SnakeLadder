d0ceb65888385c2174c09c1dfc228962a61d1fbb
Rashmi <rashmichigon65@gmail.com>
2022-11-20 09:36:24 +0530

    SnakeLadderUC2

--- Program.cs
+++ Program.cs
@@ -5,8 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Simulation");
+
+            //UC1
+
             int position = 0;
             Console.WriteLine("Initial (start) position is:" + position);
+
+            //UC2
+
+            Random random = new Random();
+            int diceOutput = random.Next(1, 7);
+            Console.WriteLine("Dice Output is :" + diceOutput);
             Console.ReadLine();
         }
     }
